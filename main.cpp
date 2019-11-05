2b7181f3917abea9a618c7342188c92cb2f162a8
isadorasalles <isadorasalles@dcc.ufmg.br>
2019-11-05 14:36:51 -0300

    Criar grafo

---
+++ main.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+#include <vector>
+#include <fstream>
+
+using namespace std;
+
+void greedy_heuristic(vector <vector <int > > G);
+
+int main(int argc, char *argv[]){
+	
+	ifstream file;
+	file.open(argv[1]);
+	int v1, v2;
+	string str;
+	int nodes, edges;
+	vector <vector <int> > M;
+	
+	//sera formado o grafo complementar de cada instancia
+	if (file.is_open()){
+		file >> nodes;
+			file >> edges;
+			for (int j = 0; j < nodes; j++){
+				vector<int> N;
+				for (int i = 0; i < nodes; i++){
+					if (i != j)
+						N.push_back(1);
+					else 
+						N.push_back(0);
+				}
+				M.push_back(N);
+			}
+		for(int k = 0; k < edges; k++){
+			cout << k << "\n";
+				file >> str;
+				file >> v1;
+				file >> v2;
+				M[v1-1][v2-1] = 0;   //vertices comecam de 1 nas instancias
+				M[v2-1][v1-1] = 0;   
+		}
+	}
+
+	greedy_heuristic(M);
+
+}
