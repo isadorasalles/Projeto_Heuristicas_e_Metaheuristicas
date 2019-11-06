4a6f6f988a682b3a04a1db337213f9c1e4323fa6
breno <breno.ms1997@gmail.com>
2019-11-06 08:51:29 -0300

    fixed floyd warshall']

--- fw_heuristic.cpp
+++ fw_heuristic.cpp
@@ -3,39 +3,63 @@
 using namespace std;
 
 int **G; //grafo global foda-se
-
+int **dist; //graph to store the result of the floyd warshall algorithm
+#define INF 99999999;
 void init_matrix(int nnodes){
   for (int i = 0; i < nnodes; i++) {
     for (int j = 0; j < nnodes; j++) {
-      G[i][j] = INT_MAX;
+      if (i==j) {
+        G[i][j] = 0;
+      }
+      else{
+        G[i][j] = INF;
+      }
+      dist[i][j] = G[i][j];
     }
   }
 }
 
 void alloc_matrix(int nnodes){
   G = (int **)malloc(nnodes * sizeof(int*));
+  dist = (int **)malloc(nnodes * sizeof(int*));
   for(int i = 0; i < nnodes; i++){
-    G[i] = (int *)malloc(nnodes * sizeof(int));
+      G[i] = (int *)malloc(nnodes * sizeof(int));
+    dist[i] = (int *)malloc(nnodes * sizeof(int));
   }
   init_matrix(nnodes);
 }
 
 void free_matrix(int nnodes){
-  for(int i = 0; i < nnodes; i++)
+  for(int i = 0; i < nnodes; i++){
     free(G[i]);
+    free(dist[i]);
+  }
   free(G);
+  free(dist);
 }
 
-
+void print_matrix(int nnodes){
+  for (int i = 0; i < nnodes; i++) {
+    for (int j = 0; j < nnodes; j++) {
+      cout << dist[i][j] << " ";
+    }
+    cout << endl;
+  }
+}
 void floyd_warshall(int nodes){
   for (int i = 0; i < nodes; i++) {
     for (int j = 0; j < nodes; j++) {
       for (int k = 0; k < nodes; k++) {
-        G[i][j] = min(G[i][j],G[i][k]+G[k][j]);
+        dist[i][j] = min(dist[i][j],dist[i][k]+dist[k][j]);
+        print_matrix(nodes);
+        cout << endl;
       }
     }
   }
 }
+
+
+
 
 /* after we run floyd_warshall, do as follows:
 1. Select a starting node
@@ -43,9 +67,17 @@ void floyd_warshall(int nodes){
 2.1 if it forms a independent set, add to solution and go to 3
 2.2 else, select the n-nd closest node
 3. repeat starting from the last node added to solution
+The heuristic returns the size of the independent set found
 */
-void heuristic(int nodes){
 
+int heuristic(int nodes, int starting_node){
+  // starting_node will be indexed starting from 0
+  vector<int> independent_set;
+  for (int i = 0; i < nodes; i++) {
+    for (int j = 0; j < nodes; j++) {
+
+    }
+  }
 }
 
 int main(int argc, char const *argv[]) {
@@ -65,9 +97,13 @@ int main(int argc, char const *argv[]) {
     iss >> e >> src >> dst;
     G[src-1][dst-1] = 1;
     G[dst-1][src-1] = 1;
+    dist[src-1][dst-1] = 1;
+    dist[dst-1][src-1] = 1;
+
   }
 
   floyd_warshall(nodes);
+
   free_matrix(nodes);
   return 0;
 }
