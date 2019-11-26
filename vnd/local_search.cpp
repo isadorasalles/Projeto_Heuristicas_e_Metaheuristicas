b3238c590f6c269ffe8e4af945c3ce46496b1020
brenomatos <breno.ms1997@gmail.com>
2019-11-26 19:54:35 -0300

    vnd

---
+++ vnd/local_search.cpp
@@ -0,0 +1,129 @@
+#include <bits/stdc++.h>
+#include <vector>
+
+using namespace std;
+
+int max_degree(vector <vector <int > > G, vector <int> visited, vector <int> Set);
+vector <int> find_neighbors(vector <vector <int> > G, int v, vector <int> Set);
+
+void two_improvement(vector <vector <int> > G, vector <int> &Set){
+
+	int improvement = 0;
+
+	int size = Set.size();
+
+	do {
+		improvement = 0;
+
+		vector <int> visited;
+		for(int i = 0; i < size; i++)
+			visited.push_back(0);
+
+		for (int i = 0; i < size; i++){
+			int max = max_degree(G, visited, Set);
+			vector <int> nodes = find_neighbors(G, max, Set);
+
+			visited[max] = 1;
+			if (nodes.size() == 2){
+				Set.erase(Set.begin()+max);
+				Set.push_back(nodes[0]);
+				Set.push_back(nodes[1]);
+				improvement = 1;
+			}	
+		}
+
+		size = Set.size();
+
+		
+	} while (improvement == 1);
+	
+}
+
+
+void three_improvement(vector <vector <int> > G, vector <int> &Set){
+
+	int improvement = 0;
+
+	int size = Set.size();
+
+	do {
+		improvement = 0;
+
+		vector <int> visited;
+		for(int i = 0; i < size; i++)
+			visited.push_back(0);
+
+		for (int i = 0; i < size; i++){
+			int max = max_degree(G, visited, Set);
+			vector <int> nodes = find_neighbors(G, max, Set);
+
+			visited[max] = 1;
+			if (nodes.size() == 3){
+				Set.erase(Set.begin()+max);
+				Set.push_back(nodes[0]);
+				Set.push_back(nodes[1]);
+				Set.push_back(nodes[2]);
+				improvement = 1;
+			}	
+		}
+
+		size = Set.size();
+
+		
+	} while (improvement == 1);
+	
+}
+
+int max_degree(vector <vector <int > > G, vector <int> visited, vector <int> Set){
+	int max = 0;
+	int index = 0;
+	int sum = 0;
+	for(int i = 0; i < Set.size(); i++){
+		if (visited[i] == 0){
+			for (int j = 0; j < G.size(); j++){
+				if (visited[j] == 0)
+					sum += G[Set[i]][j];
+			}
+			if (sum > max){
+				max = sum;
+				index = i;
+			}
+			sum = 0;
+		}
+	}
+	return index;
+}
+
+vector <int> find_neighbors(vector <vector <int> > G, int v, vector <int> Set){
+	vector <int>::iterator it;
+	vector <int> nodes;
+	vector <int> neighbors;
+	int count = 0;
+
+	for (int i = 0; i < G.size(); i++){
+		if (G[Set[v]][i] == 1)
+  			neighbors.push_back(i);
+	}
+
+	for (int i = 0; i < neighbors.size(); i++){
+		for (int j = 0; j < Set.size(); j++){
+			if (j!=v){
+				if (G[neighbors[i]][Set[j]] == 1){
+					count++;
+		  		}
+		  		if ((nodes.size() == 1)&&(G[neighbors[i]][nodes[0]] == 1))
+		  			count++;
+		  	}
+		}
+		if (count == 0){ 
+			it = find (Set.begin(), Set.end(), neighbors[i]);
+			if (it == Set.end())
+				nodes.push_back(neighbors[i]);
+		}
+		count = 0;
+		if (nodes.size() == 2)
+			break;
+	}
+
+	return nodes;
+}
---
+++ vnd/vnd.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#include <vector>
+#include <cstdio>
+#include <ctime>
+#include <time.h> 
+#include <algorithm>
+#define TABU 555
+#define LIST_SIZE 57
+
+using namespace std;
+
+vector <int> greedy_heuristic(vector <vector <int > > G);
+void two_improvement(vector <vector <int> > G, vector <int> &Set);
+void three_improvement(vector <vector <int> > G, vector <int> &Set);
+
+
+void vnd(vector <vector <int> > G){
+	vector <int> S = greedy_heuristic(G);
+	vector <int> aux, best_solution;
+	int size, best_size = (int)S.size();
+
+	two_improvement(G, S);
+	three_improvement(G, S);
+
+	std::cout << S.size() << std::endl; 
+}
